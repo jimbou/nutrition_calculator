a0e3e73c39904d2c718bebcf2a6f3cde2b2bc1e3
jimbou <el17072@mail.ntua.gr>
2023-05-06 14:04:02 +0300

    Add specific calories per meal support

--- src/meal_calories.py
+++ src/meal_calories.py
@@ -1,4 +1,20 @@
-def run():
+def input_type():
+    while True:
+        result = str(input("\n Θες ποσοτά ή αριθμό θερμιδών ; ")).lower()
+
+        if result[0] in [ "π", "Π", "p", "P","1"]:  
+            return "percentage"
+
+        elif result[0] in ["A", "a",  "Α", "α","θ","Θ","2"]:  
+            return "calories"
+
+        else:
+            print(f"Λάθος είσοδος: {result} ")
+            continue
+
+
+def run_perc():
+
     total_calories = int(input("Δώσε τον ημερίσιο στόχο σου σε θερμίδες: "))
     meal_percentages = [0, 0, 0, 0, 0]  # Initialize meal percentage list with zeros
     meal =["πρωινό","σνακ","μεσημεριανό","απογευματινό","βραδινό"]
@@ -16,3 +32,31 @@ def run():
 
     meal_calories = [int(total_calories * p / 100) for p in meal_percentages]  # Calculate the number of calories for each meal
     print(meal_calories)
+
+def run_cal():
+
+    total_calories = int(input("Δώσε τον ημερίσιο στόχο σου σε θερμίδες: "))
+    meal_calories = [0, 0, 0, 0, 0]  # Initialize meal percentage list with zeros
+    meal =["πρωινό","σνακ","μεσημεριανό","απογευματινό","βραδινό"]
+    for i in range(4):
+        remaining_calories = total_calories - sum(meal_calories)
+        meal_cal = float(input(f"Δώσε τον αριθμό θερμίσων που θες να καταναλώσεις στο γευμα : {meal[i]} (Σου απομένει  {remaining_calories}% ): "))
+        while meal_cal > remaining_calories:
+            print(f"Σφαλμα: το άθροισμα των ποσοστών θερμιδών ξεπερνά το συνολικο ({total_calories}). Σου απομένει {remaining_calories}% .")
+            meal_cal = float(input(f"Δώσε τον αριθμό θερμίσων που θες να καταναλώσεις στο γευμα :{meal[i]} (Σου απομένει  {remaining_calories}% ): "))
+        meal_calories[i] = meal_cal
+
+    remaining_calories = total_calories - sum(meal_calories)
+    print(f"Για το τελευταίο γεύμα σας αναθέτουμε το  υπολοιπώμενον αριθμό θερμίδων: {remaining_calories} %")
+    meal_calories[4] = remaining_calories  # Assign remaining percentage to the final meal
+
+   
+    print(meal_calories)
+
+
+def run():
+    result = input_type() 
+    if result == "percentage":
+        run_perc()
+    else :
+        run_cal()
